17cb0796463d635e01c34d69c0f6f955db41988e
qinixiangyang111 <SpringF1_Qin@163.com>
2020-02-16 10:12:34 +0800

    "project test"

---
+++ src/main/java/com/yangyue/design/Hello.java
@@ -0,0 +1,13 @@
+package com.yangyue.design;
+
+/**
+ * @program: design
+ * @description:
+ * @author: yueyang
+ * @create: 2020-02-16 10:05
+ **/
+public class Hello {
+    public static void main(String[] args) {
+         System.out.println("Hello World");
+    }
+}
